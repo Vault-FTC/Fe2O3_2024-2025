dea4f5208cf2a68abeaba8f1507f956704743c04
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2024-11-03 09:44:10 -0500

    set up controls for scrimmage #2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/Tele.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.commands.DriveDefault;
+import org.firstinspires.ftc.teamcode.commands.GroundIntakeServo;
+import org.firstinspires.ftc.teamcode.commands.IntakeSlideToPosition;
+import org.firstinspires.ftc.teamcode.commands.LiftIntakeServo;
 import org.firstinspires.ftc.teamcode.commands.RunExtrudingArm;
 import org.firstinspires.ftc.teamcode.commands.RunIntake;
 import org.firstinspires.ftc.teamcode.constants.SubsystemConstants;
@@ -26,15 +29,23 @@ public class Tele extends Robot implements OpModeCore {
         if (alliance == Alliance.RED) {
             backdropPose = redBackdropPose;
         }
+        drive.enableFastMode();
 
         drive.setDefaultCommand(new DriveDefault(drive, () -> -controller1.leftStickY.getAsDouble(), () -> controller1.leftStickX.getAsDouble(), () -> -controller1.rightStickX.getAsDouble()));
         controller1.b.and(controller1.x).and(controller1.y).onTrue(new InstantCommand(() -> drive.getOdometry().setPosition(new Pose2d())));
+        controller1.a.and(controller1.b).andNot(controller1.x).andNot(controller1.y).onTrue(new InstantCommand(() -> drive.enableFastMode()));
+        controller1.a.and(controller1.x).andNot(controller1.b).andNot(controller1.y).onTrue(new InstantCommand(() -> drive.enableSlowMode()));
+
         controller1.leftTrigger.andNot(controller1.rightTrigger).onTrue(new RunIntake(intake, SubsystemConstants.Intake.IntakeModes.OUTTAKE.speed));
         controller1.rightTrigger.andNot(controller1.leftTrigger).onTrue(new RunIntake(intake, SubsystemConstants.Intake.IntakeModes.INTAKE.speed));
         controller1.leftTrigger.andNot(controller1.rightTrigger).onFalse(new RunIntake(intake, SubsystemConstants.Intake.IntakeModes.OFF.speed));
         controller1.rightTrigger.andNot(controller1.leftTrigger).onFalse(new RunIntake(intake, SubsystemConstants.Intake.IntakeModes.OFF.speed));
-        controller1.a.and(controller1.b).andNot(controller1.x).andNot(controller1.y).onTrue(new InstantCommand(() -> drive.enableFastMode()));
-        controller1.a.and(controller1.x).andNot(controller1.b).andNot(controller1.y).onTrue(new InstantCommand(() -> drive.enableSlowMode()));
+
+        controller1.leftBumper.andNot(controller1.rightBumper).onTrue(new IntakeSlideToPosition(intakeSlide, SubsystemConstants.IntakeSlide.prepareIntakePosition));
+        controller1.rightBumper.andNot(controller1.leftBumper).onTrue(new IntakeSlideToPosition(intakeSlide, SubsystemConstants.IntakeSlide.stowedPosition));
+
+        controller1.dpadDown.andNot(controller1.dpadUp).onTrue(new GroundIntakeServo(intake));
+        controller1.dpadUp.andNot(controller1.dpadDown).onTrue(new LiftIntakeServo(intake));
     }
 
     @Override
@@ -42,6 +53,9 @@ public class Tele extends Robot implements OpModeCore {
         telemetry.addData("heading", drive.getOdometry().getPosition().rotation.getAngleDegrees());
         telemetry.addData("Input 1", Rustboard.getString("input_1", ""));
         telemetry.addData("active rustboard uuid", Rustboard.getActiveRustboard().getUuid());
+        telemetry.addData("IntakeSlide Position", intakeSlide.encoder.getPosition());
+        telemetry.addData("Servo1 position",intake.servo1.getPosition());
+        telemetry.addData("Servo2 position",intake.servo2.getPosition());
         //Rustboard.notifyActiveClient("op mode running", NoticeType.POSITIVE);
     }
 }
