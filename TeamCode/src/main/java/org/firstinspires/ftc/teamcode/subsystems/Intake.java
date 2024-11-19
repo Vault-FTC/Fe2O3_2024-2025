2ea2c6cf78144b0e1e1e18388cac3585d579c306
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2024-11-19 18:32:39 -0500

    updated Intake to be a CRServo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,25 +1,30 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 import com.qualcomm.robotcore.util.Range;
 
 import org.rustlib.commandsystem.Subsystem;
+import org.rustlib.config.HardwareConfiguration;
 
 public class Intake extends Subsystem {
-    public final DcMotor motor;
+//    public final DcMotor motor;
     public final Servo servo1;
+    public final CRServo crServo;
     public final Servo servo2;
 
     public Intake(HardwareMap hardwareMap) {
-        this.motor = hardwareMap.dcMotor.get("intake");
         this.servo1 = hardwareMap.servo.get("intakeServo1");
         this.servo2 = hardwareMap.servo.get("intakeServo2");
+        this.crServo = hardwareMap.crservo.get("intake");
     }
 
     public void run(double speed) {
-        motor.setPower(Range.clip(speed, -1.0, 1.0));
+        crServo.setPower(Range.clip(speed, -1.0, 1.0));
     }
 
     public void groundedPosition(){
