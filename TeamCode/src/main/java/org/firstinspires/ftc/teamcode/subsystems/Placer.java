fef07bf2da27a094307cfd60ef024b8985da324c
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2025-01-25 07:00:39 -0500

    disabled distanceSensor on Placer as it was causing a significant delay in control of the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Placer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Placer.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.constants.SubsystemConstants;
@@ -11,36 +13,36 @@ import org.rustlib.commandsystem.Subsystem;
 import org.rustlib.rustboard.Rustboard;
 
 public class Placer extends Subsystem {
-    public final TouchSensor touchSensor;
-    private final Servo lifter0;
-    private final Servo lifter1;
+//    public final TouchSensor touchSensor;
+    private final CRServo lifter0;
+    private final CRServo lifter1;
     private final Servo placer;
-    public DistanceSensor distanceSensor;
+//    public DistanceSensor distanceSensor;
 
     public Placer(HardwareMap hardwareMap) {
-        lifter0 = hardwareMap.get(Servo.class, "lifter0");
-        lifter0.setDirection(Servo.Direction.FORWARD);
+        lifter0 = hardwareMap.get(CRServo.class, "lifter0");
+        lifter0.setDirection(CRServo.Direction.FORWARD);
         lifter0.close();
-        lifter1 = hardwareMap.get(Servo.class, "lifter1");
-        lifter1.setDirection(Servo.Direction.FORWARD);
+        lifter1 = hardwareMap.get(CRServo.class, "lifter1");
+        lifter1.setDirection(CRServo.Direction.FORWARD);
         lifter1.close();
         placer = hardwareMap.get(Servo.class, "placer");
         placer.setDirection(Servo.Direction.FORWARD);
         placer.close();
-        distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
-        touchSensor = hardwareMap.get(TouchSensor.class, "placerTouch");
+//        distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+//        touchSensor = hardwareMap.get(TouchSensor.class, "placerTouch");
     }
 
-    public void placePosition() {
-        lifter0.setPosition(SubsystemConstants.Placer.lifter0PlacePosition);
-        lifter1.setPosition(SubsystemConstants.Placer.lifter1PlacePosition);
+    public void runTilt(double speed) {
+        lifter0.setPower(Range.clip(speed, -SubsystemConstants.Placer.defaultSpeed, SubsystemConstants.Placer.defaultSpeed));
+        lifter1.setPower(Range.clip(-speed, -SubsystemConstants.Placer.defaultSpeed, SubsystemConstants.Placer.defaultSpeed));
     }
 
-    public void storagePosition() {
-        lifter0.setPosition(SubsystemConstants.Placer.lifter0StoragePosition);
-        lifter1.setPosition(SubsystemConstants.Placer.lifter1StoragePosition);
-        placer.setPosition(SubsystemConstants.Placer.closePosition);
-    }
+//    public void storagePosition() {
+//        lifter0.setPower(SubsystemConstants.Placer.lifter0PlacePosition);
+//        lifter1.setPower(SubsystemConstants.Placer.lifter1PlacePosition);
+//        placer.setPosition(SubsystemConstants.Placer.closePosition);
+//    }
 
     public void open() {
         placer.setPosition(SubsystemConstants.Placer.openPosition);
@@ -50,12 +52,12 @@ public class Placer extends Subsystem {
         placer.setPosition(SubsystemConstants.Placer.closePosition);
     }
 
-    public double getDistance() {
-        return distanceSensor.getDistance(DistanceUnit.INCH);
-    }
+//    public double getDistance() {
+//        return distanceSensor.getDistance(DistanceUnit.INCH);
+//    }
 
-    @Override
-    public void periodic() {
-        Rustboard.updateTelemetryNode("distance", getDistance());
-    }
+//    @Override
+//    public void periodic() {
+//        Rustboard.updateTelemetryNode("distance", getDistance());
+//    }
 }
