37015731449cf7bc0686b3cd88134ffa161ca420
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2025-01-25 07:02:19 -0500

    commented out because the placer sensors are disabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BackdropAlign.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BackdropAlign.java
@@ -1,36 +1,36 @@
-package org.firstinspires.ftc.teamcode.commands;
-
-import org.firstinspires.ftc.teamcode.subsystems.Placer;
-import org.rustlib.commandsystem.Command;
-import org.rustlib.drive.DriveSubsystem;
-import org.rustlib.drive.Waypoint;
-import org.rustlib.geometry.Pose2d;
-import org.rustlib.geometry.Rotation2d;
-import org.rustlib.rustboard.Rustboard;
-
-public class BackdropAlign extends Command {
-    private final DriveSubsystem drive;
-    private final Placer placer;
-    private final double timeout;
-    private double backdropOffset = 0;
-
-    public BackdropAlign(DriveSubsystem drive, Placer placer, double timeout) {
-        this.drive = drive;
-        this.placer = placer;
-        this.timeout = timeout;
-    }
-
-    @Override
-    public void execute() {
-        Pose2d botPose = drive.getOdometry().getPosition();
-        backdropOffset = Rustboard.getDouble("place offset", 3.5);
-        drive.getBase().driveToPosition(new Waypoint(botPose.x - (placer.getDistance() - backdropOffset), botPose.y, 0, Rotation2d.fromDegrees(-90), Rotation2d.fromDegrees(-90), 0.5));
-    }
-
-    @Override
-    public boolean isFinished() {
-        return Math.abs(Rotation2d.getError(Math.toRadians(270), drive.getOdometry().getPosition().rotation.getAngleRadians())) < Math.toRadians(2.5)
-                && Math.abs(placer.getDistance() - backdropOffset) < 0.5
-                || timeSinceInitialized() > timeout;
-    }
-}
+//package org.firstinspires.ftc.teamcode.commands;
+//
+//import org.firstinspires.ftc.teamcode.subsystems.Placer;
+//import org.rustlib.commandsystem.Command;
+//import org.rustlib.drive.DriveSubsystem;
+//import org.rustlib.drive.Waypoint;
+//import org.rustlib.geometry.Pose2d;
+//import org.rustlib.geometry.Rotation2d;
+//import org.rustlib.rustboard.Rustboard;
+//
+//public class BackdropAlign extends Command {
+//    private final DriveSubsystem drive;
+//    private final Placer placer;
+//    private final double timeout;
+//    private double backdropOffset = 0;
+//
+//    public BackdropAlign(DriveSubsystem drive, Placer placer, double timeout) {
+//        this.drive = drive;
+//        this.placer = placer;
+//        this.timeout = timeout;
+//    }
+//
+//    @Override
+//    public void execute() {
+//        Pose2d botPose = drive.getOdometry().getPosition();
+//        backdropOffset = Rustboard.getDouble("place offset", 3.5);
+//        drive.getBase().driveToPosition(new Waypoint(botPose.x - (placer.getDistance() - backdropOffset), botPose.y, 0, Rotation2d.fromDegrees(-90), Rotation2d.fromDegrees(-90), 0.5));
+//    }
+//
+//    @Override
+//    public boolean isFinished() {
+//        return Math.abs(Rotation2d.getError(Math.toRadians(270), drive.getOdometry().getPosition().rotation.getAngleRadians())) < Math.toRadians(2.5)
+//                && Math.abs(placer.getDistance() - backdropOffset) < 0.5
+//                || timeSinceInitialized() > timeout;
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BackdropHome.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BackdropHome.java
@@ -1,58 +1,58 @@
-package org.firstinspires.ftc.teamcode.commands;
-
-import org.firstinspires.ftc.teamcode.subsystems.Placer;
-import org.firstinspires.ftc.teamcode.subsystems.Slide;
-import org.rustlib.commandsystem.Command;
-import org.rustlib.drive.MecanumBase;
-import org.rustlib.drive.Waypoint;
-
-import java.util.function.Supplier;
-
-public class BackdropHome extends Command {
-    private final MecanumBase base;
-    private final Slide slide;
-    private final Placer placer;
-    private final Supplier<Waypoint> futureBackdropWaypoint;
-    private final double followTimeout;
-    private final double endTime;
-    private Waypoint backdropWaypoint;
-    private boolean atWaypoint = false;
-    private boolean lastAtWaypoint = false;
-    private double timestamp = -1;
-
-    public BackdropHome(MecanumBase base, Slide slide, Placer placer, Supplier<Waypoint> futureBackdropWaypoint, double followTimeout, double endTime) {
-        this.futureBackdropWaypoint = futureBackdropWaypoint;
-        this.base = base;
-        this.slide = slide;
-        this.placer = placer;
-        this.followTimeout = followTimeout;
-        this.endTime = endTime;
-    }
-
-    @Override
-    public void initialize() {
-        atWaypoint = false;
-        lastAtWaypoint = false;
-        backdropWaypoint = futureBackdropWaypoint.get();
-    }
-
-    @Override
-    public void execute() {
-        base.driveToPosition(backdropWaypoint);
-    }
-
-    @Override
-    public boolean isFinished() {
-        lastAtWaypoint = atWaypoint;
-        atWaypoint = base.atWaypoint(backdropWaypoint, 0.5, 5)
-                || timeSinceInitialized() > followTimeout
-                || placer.touchSensor.isPressed()
-                || (placer.getDistance() < 4.0 && slide.encoder.getTicks() > 300);
-
-        if (atWaypoint && !lastAtWaypoint) {
-            timestamp = timeSinceInitialized();
-        }
-
-        return atWaypoint && timeSinceInitialized() > timestamp + endTime;
-    }
-}
+//package org.firstinspires.ftc.teamcode.commands;
+//
+//import org.firstinspires.ftc.teamcode.subsystems.Placer;
+//import org.firstinspires.ftc.teamcode.subsystems.Slide;
+//import org.rustlib.commandsystem.Command;
+//import org.rustlib.drive.MecanumBase;
+//import org.rustlib.drive.Waypoint;
+//
+//import java.util.function.Supplier;
+//
+//public class BackdropHome extends Command {
+//    private final MecanumBase base;
+//    private final Slide slide;
+//    private final Placer placer;
+//    private final Supplier<Waypoint> futureBackdropWaypoint;
+//    private final double followTimeout;
+//    private final double endTime;
+//    private Waypoint backdropWaypoint;
+//    private boolean atWaypoint = false;
+//    private boolean lastAtWaypoint = false;
+//    private double timestamp = -1;
+//
+//    public BackdropHome(MecanumBase base, Slide slide, Placer placer, Supplier<Waypoint> futureBackdropWaypoint, double followTimeout, double endTime) {
+//        this.futureBackdropWaypoint = futureBackdropWaypoint;
+//        this.base = base;
+//        this.slide = slide;
+//        this.placer = placer;
+//        this.followTimeout = followTimeout;
+//        this.endTime = endTime;
+//    }
+//
+//    @Override
+//    public void initialize() {
+//        atWaypoint = false;
+//        lastAtWaypoint = false;
+//        backdropWaypoint = futureBackdropWaypoint.get();
+//    }
+//
+//    @Override
+//    public void execute() {
+//        base.driveToPosition(backdropWaypoint);
+//    }
+//
+//    @Override
+//    public boolean isFinished() {
+//        lastAtWaypoint = atWaypoint;
+//        atWaypoint = base.atWaypoint(backdropWaypoint, 0.5, 5)
+//                || timeSinceInitialized() > followTimeout
+//                || placer.touchSensor.isPressed()
+//                || (placer.getDistance() < 4.0 && slide.encoder.getTicks() > 300);
+//
+//        if (atWaypoint && !lastAtWaypoint) {
+//            timestamp = timeSinceInitialized();
+//        }
+//
+//        return atWaypoint && timeSinceInitialized() > timestamp + endTime;
+//    }
+//}
