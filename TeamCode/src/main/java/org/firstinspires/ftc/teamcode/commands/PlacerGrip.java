12d408ca0d57f6850f5a2192043b580af551450f
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2025-01-27 19:25:27 -0500

    Created PlacerGrip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/PlacerGrip.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import org.firstinspires.ftc.teamcode.subsystems.Placer;
+import org.rustlib.commandsystem.Command;
+
+import java.util.function.DoubleSupplier;
+
+public class PlacerGrip extends Command {
+    public Placer placer;
+    public DoubleSupplier positionSupplier;
+    public PlacerGrip(Placer placer, DoubleSupplier position){
+        this.placer = placer;
+        this.positionSupplier = position;
+        placer.setGripPosition(position.getAsDouble());
+        addRequirements(this.placer);
+    }
+}
